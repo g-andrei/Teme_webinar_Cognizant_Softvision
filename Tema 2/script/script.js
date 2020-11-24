59a2dbfb56bd6635814109c2ffd91736b208ecca
g-andrei <andrei_gheorghiu95@yahoo.com>
2020-11-24 14:17:36 +0200

    l-am adaugat pe george

--- Tema 2/script/script.js
+++ Tema 2/script/script.js
@@ -5,26 +5,43 @@
 
 // console.log(iLoveJs);
 
+// let anotherArray = ['Paul', 
+//                     1, 
+//                     false, 
+//                     {name: 'Jon Snow'}, 
+//                     [1, 2, 3], 
+//                     null, 
+//                     undefined, 
+//                     function() { 
+//                         console.log('Test');
+//                     },
+// ];
 
 
+// for(let i = 0; i < anotherArray.length; i++) {
+//     console.log(`Pozitia: ${i}, 
+// valoarea: ${anotherArray[i]},
+// tipul: ${typeof anotherArray[i]}.
 
-let anotherArray = ['Paul', 
-                    1, 
-                    false, 
-                    {name: 'Jon Snow'}, 
-                    [1, 2, 3], 
-                    null, 
-                    undefined, 
-                    function() { 
-                        console.log('Test');
-                    },
-];
+// `);
+// }
 
 
-for(let i = 0; i < anotherArray.length; i++) {
-    console.log(`Pozitia: ${i}, 
-valoarea: ${anotherArray[i]},
-tipul: ${typeof anotherArray[i]}.
+const canvas = document.getElementById('canvasId');
+/** @type {CanvasRenderingContext2D} */
+const context = canvas.getContext('2d');
 
-`);
+context.fillStyle = "red";
+context.fillRect(100, 20, 20, 5);
+
+// ################### GEORGE ###################
+
+const george = new Image();
+george.src = './assets/george.png';
+
+const GEORGE_WIDTH = 40;
+const GEORGE_HEIGHT = 45;
+
+george.onload = () => {
+    context.drawImage(george, 0, GEORGE_HEIGHT, GEORGE_WIDTH, GEORGE_HEIGHT, 100, 100, GEORGE_WIDTH, GEORGE_HEIGHT);
 }
